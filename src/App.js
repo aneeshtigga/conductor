b7f37526e2af99d59e69386a949603c930258f84
Aneesh Tigga <aneeshtigga@gmail.com>
2024-09-18 12:18:21 +0530

    1.0.10 remove bar and change colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Line, Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import html2canvas from 'html2canvas';
 import logo from './stars.svg';
@@ -110,13 +110,10 @@ function App() {
     window.removeEventListener('mouseup', stopResizing);
   };
 
-  const val = Bar;
-
   return (
-    <div className="flex">
-      {/* Sidebar for selecting datasets */}
+    <div className="flex bg-gray-50 h-screen text-gray-800">
       <div
-        className="h-screen bg-gray-200 text-black p-4 overflow-y-auto fixed"
+        className="h-screen bg-white text-black p-4 shadow-md overflow-y-auto fixed"
         style={{ width: sidebarWidth }}
       >
         <h2 className="text-1xl font-bold mb-4">Datasets</h2>
@@ -125,13 +122,13 @@ function App() {
           placeholder="Search by Name"
           value={searchQuery}
           onChange={e => setSearchQuery(e.target.value)}
-          className="border border-gray-200 rounded-md p-2 w-full mb-4"
+          className="border border-gray-300 rounded-md p-2 w-full mb-4"
         />
         <ul>
           {filteredDatasets.map(dataset => (
             <li
               key={dataset.id}
-              className={`p-2 mb-2 cursor-pointer rounded-md select-none ${selectedDatasets.includes(dataset.name) ? 'bg-gray-400' : 'bg-gray-300 hover:bg-gray-400'}`}
+              className={`p-2 mb-2 cursor-pointer rounded-md select-none ${selectedDatasets.includes(dataset.name) ? 'bg-blue-200' : 'bg-white hover:bg-blue-100'}`}
               onClick={() => toggleDataset(dataset.name)}
               style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
             >
@@ -147,99 +144,92 @@ function App() {
         style={{ width: '5px', marginLeft: sidebarWidth }}
       ></div>
 
-      {/* Main content area as chat-style */}
-      <div className="flex-1 p-6 overflow-auto chat-container" style={{marginLeft: `${sidebarWidth + 5}px` }}>
+      <div className="flex-1 p-6 overflow-auto" style={{marginLeft: `${sidebarWidth + 5}px` }}>
         <div className="flex justify-center items-center mb-6">
-          <img src={logo} alt="App Logo" className="h-10 w-10" />
-          <h1 className="text-3xl font-bold">Data Insights</h1>
+          <img src={logo} alt="App Logo" className="h-10 w-10 mr-2" />
+          <h1 className="text-3xl font-bold text-blue-600">Data Insights</h1>
         </div>
 
-        {/* Chat Messages */}
         <div className="chat-messages space-y-4 mb-8">
-        {messages.map((message, index) => (
-          <div key={index} className="p-4 bg-gray-100 rounded-md">
-            {/* Graph Section */}
-            <div className="flex justify-between items-center mb-4 mt-4">
-              <h3 className="text-lg font-semibold">Graph</h3>
-              <i
-                className="fas fa-download text-gray-600 cursor-pointer hover:text-blue-500"
-                onClick={downloadGraphAsImage}
-                title="Download Graph as PNG"
-              />
-            </div>
-            <div className="chart-container h-96">
-              {message.graphLabels.length > 0 && message.graphDataset.length > 0 && (
-                <Line
-                  data={{
-                    labels: message.graphLabels,
-                    datasets: message.graphDataset
-                  }}
-                  options={{
-                    responsive: true,
-                    maintainAspectRatio: false
-                  }}
+          {messages.map((message, index) => (
+            <div key={index} className="bg-white shadow-md rounded-lg p-6">
+              <div className="flex justify-between items-center mb-4 mt-4">
+                <h3 className="text-lg font-semibold">Graph</h3>
+                <i
+                  className="fas fa-download text-gray-500 cursor-pointer hover:text-blue-500"
+                  onClick={downloadGraphAsImage}
+                  title="Download Graph as PNG"
                 />
-              )}
-            </div>
+              </div>
+              <div className="chart-container h-96">
+                {message.graphLabels.length > 0 && message.graphDataset.length > 0 && (
+                  <Line
+                    data={{
+                      labels: message.graphLabels,
+                      datasets: message.graphDataset
+                    }}
+                    options={{
+                      responsive: true,
+                      maintainAspectRatio: false
+                    }}
+                  />
+                )}
+              </div>
 
-            {/* Table Section */}
-            <div className="flex justify-between items-center mb-4 mt-4">
-              <h3 className="text-lg font-semibold">Table Data</h3>
-              <i
-                className="fas fa-file-export text-gray-600 cursor-pointer hover:text-blue-500"
-                onClick={downloadTableAsCSV}
-                title="Export Table as CSV"
-              />
-            </div>
-            <div style={{ overflowX: 'auto' }}>
-              <table className="min-w-full bg-white border border-gray-300 rounded-md mb-4">
-                <thead className="bg-gray-200 text-gray-600">
-                  <tr>
-                    {message.tableHeaders.map((header, i) => (
-                      <th key={i} className="py-2 px-4 border-b">{header}</th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {message.tableData.map((row, rowIndex) => (
-                    <tr key={rowIndex} className="text-center">
-                      {Object.values(row).map((value, colIndex) => (
-                        <td key={colIndex} className="py-2 px-4 border-b">{value}</td>
+              <div className="flex justify-between items-center mb-4 mt-4">
+                <h3 className="text-lg font-semibold">Table Data</h3>
+                <i
+                  className="fas fa-file-export text-gray-500 cursor-pointer hover:text-blue-500"
+                  onClick={downloadTableAsCSV}
+                  title="Export Table as CSV"
+                />
+              </div>
+              <div style={{ overflowX: 'auto' }}>
+                <table className="min-w-full bg-gray-100 border border-gray-300 rounded-md mb-4">
+                  <thead className="bg-gray-200 text-gray-600">
+                    <tr>
+                      {message.tableHeaders.map((header, i) => (
+                        <th key={i} className="py-2 px-4 border-b">{header}</th>
                       ))}
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-
-            {/* Query Section */}
-            <div className={`flex-1 overflow-y-auto ${isFullScreen ? 'fixed inset-0 bg-white z-50 p-8' : 'max-h-[calc(100vh-400px)]'}`}>
-              <div className="flex justify-between items-center mb-4  mt-4">
-                <h3 className="text-lg font-semibold">Query</h3>
-                <div className="flex items-center space-x-2">
-                  <i
-                    className="fas fa-copy text-gray-600 cursor-pointer hover:text-blue-500"
-                    onClick={copyQueryToClipboard}
-                    title="Copy Query to Clipboard"
-                  />
-                  <button
-                    onClick={() => setIsFullScreen(!isFullScreen)}
-                    className="text-gray-600 underline hover:text-blue-500"
-                  >
-                    {isFullScreen ? <i className="fa-solid fa-compress" title="Collapse"/> : <i className="fa-solid fa-expand" title="Fullscreen"/>}
-                  </button>
-                </div>
+                  </thead>
+                  <tbody>
+                    {message.tableData.map((row, rowIndex) => (
+                      <tr key={rowIndex} className="text-center">
+                        {Object.values(row).map((value, colIndex) => (
+                          <td key={colIndex} className="py-2 px-4 border-b">{value}</td>
+                        ))}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
-              <pre className={`bg-gray-100 p-4 rounded-md border border-gray-300 overflow-x-auto overflow-y-auto ${isFullScreen ? 'max-h-100' : 'max-h-96'}`}>
-                {message.query}
-              </pre>
+
+              <div className={`flex-1 overflow-y-auto ${isFullScreen ? 'fixed inset-0 bg-white z-50 p-8' : 'max-h-[calc(100vh-400px)]'}`}>
+                <div className="flex justify-between items-center mb-4 mt-4">
+                  <h3 className="text-lg font-semibold">Query</h3>
+                  <div className="flex items-center space-x-2">
+                    <i
+                      className="fas fa-copy text-gray-500 cursor-pointer hover:text-blue-500"
+                      onClick={copyQueryToClipboard}
+                      title="Copy Query to Clipboard"
+                    />
+                    <button
+                      onClick={() => setIsFullScreen(!isFullScreen)}
+                      className="text-gray-500 underline hover:text-blue-500"
+                    >
+                      {isFullScreen ? <i className="fa-solid fa-compress" title="Collapse"/> : <i className="fa-solid fa-expand" title="Fullscreen"/>}
+                    </button>
+                  </div>
+                </div>
+                <pre className={`bg-gray-100 p-4 rounded-md border border-gray-300 overflow-x-auto overflow-y-auto ${isFullScreen ? 'max-h-100' : 'max-h-96'}`}>
+                  {message.query}
+                </pre>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
 
-
-        {/* Input for description */}
         <div className="input-container flex justify-center mb-8">
           <input
             type="text"
