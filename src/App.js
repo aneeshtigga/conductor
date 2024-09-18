5791d4008ee2622db19246dc961359d844c94143
Aneesh Tigga <aneeshtigga@gmail.com>
2024-09-18 14:04:13 +0530

    1.0.11 eslint handle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Line } from 'react-chartjs-2';
+import { Line, Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import html2canvas from 'html2canvas';
 import logo from './stars.svg';
